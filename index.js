7b6649ff8f05d15e255d4be96f59e8f628551b20
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-04-06 17:10:32 +0300

    countries

--- index.js
+++ index.js
@@ -19,4 +19,31 @@ async function getCountry() {
         console.error(err);
     }
 }
+getCountry()
+function showCountry(data) {
+    const country = document.createElement("div");
+    country.classList.add("country")
+    country.innerHTML = `  <div class="country-img">
+    <img src="${data.flags.png}"alt="">
+</div>
+<div class="country-info">
+    <h5 class="countryName">${data.name.common}</h5>
+    <p><strong>Population:</strong> ${data.population}</p>
+    <p class="regionName"><strong>Region:</strong> ${data.region}</p>
+    <p><strong>Capital:</strong> ${data.capital}</p>
+    <p><strong>Time:</strong> ${data.timezones}</p>
+    <p><strong>Days:</strong> ${data.startOfWeek}</p>
+</div>`;
+    countriesElem.appendChild(country)
+    country.addEventListener("click", () => {
+        showCountryDetail(data)
+    })
+}
+
+// dropDown.addEventListener("click", () => {
+//     dropElem.classList.toggle("showDropDown")
+    
+//     // console.log("dropDown")
+// })
+
 
