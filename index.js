78502e4f360d7a542d1598d373f104b4f0154883
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-04-06 17:11:16 +0300

    countries

--- index.js
+++ index.js
@@ -45,5 +45,68 @@ function showCountry(data) {
     
 //     // console.log("dropDown")
 // })
+const regionName = document.getElementsByClassName("regionName")
+const countryName = document.getElementsByClassName("countryName")
+region.forEach(element => {
+    element.addEventListener("click", () => {
+        console.log(element)
+        Array.from(regionName).forEach(elem => {
+            console.log(elem.innerText)
+            if (elem.innerText.includes(element.innerText) || element.innerText == "All") {
+                elem.parentElement.parentElement.style.display = "grid"
+            } else {
+                elem.parentElement.parentElement.style.display = "none"
+            }
+        });
+    });
+});
+
+search.addEventListener("input", () => {
+    console.log(search.value.toLowerCase());
+    Array.from(countryName).forEach(elem => {
+        if (elem.innerText.toLowerCase().includes(search.value.toLowerCase())) {
+            elem.parentElement.parentElement.style.display = "grid"
+        } else {
+            elem.parentElement.parentElement.style.display = "none"
+        }
+    });
+})
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+    moon.classList.toggle("fas")
+})
+
+const countryModal = document.querySelector(".countryModal");
+function showCountryDetail(data) {
+    countryModal.classList.toggle("show")
+    countryModal.innerHTML = `<button class="back">Back</button>
+    <div class="modal">
+        <div class="leftModal">
+            <img src="${data.flags.png}" alt="">
+        </div>
+        <div class="rightModal">
+            <h2>${data.name.map(elem=>elem.common.official)}</h2>
+           <div class="modelInfo">
+            <div class="innerLeft inner">
+                <p><strong>Native Name:</strong> ${data.name.common}</p>
+                <p><strong>Population:</strong> ${data.population}</p>
+                <p><strong>Region:</strong> ${data.region}</p>
+                <p><strong>Sub-region:</strong> ${data.subregion}</p>
+        </div>
+        <div class="innerRight inner">
+                <p><strong>Capital:</strong> ${data.capital}</p>
+                <p><strong>Top Level Domain:</strong> ${data.topLevelDomain.map(elem => elem)}</p>
+                <p><strong>Currencies:</strong> ${data.currencies.map(elem => elem.name)}</p>
+                <p><strong>Languages:</strong> ${data.language.map(elem => elem.name)}</p>
+         </div>
+        </div>
+    </div>
+</div>`
+    
+}
+const back = countryModal.querySelector(".back")
+    back.addEventListener("click", () => {
+        countryModal.classList.toggle("show")
+    })
 
 
